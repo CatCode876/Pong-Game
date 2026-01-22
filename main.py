8368c6ea8e32af782df38d2bbf65811ac26ddf8b
Cat <johncat156@gmail.com>
2026-01-22 07:24:36 -0800

    Working on ball

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from pygame import *
 
 screen = display.set_mode((0, 0), FULLSCREEN)
+info = display.Info()
+WIDTH = info.current_w
+HEIGHT = info.current_h
+
+CENTER_WALLX = WIDTH // 2
+CENTER_WALLY = HEIGHT // 2
 display.set_caption("Pong Game")
 WHITE = (255, 255, 255)
 class Player(sprite.Sprite):
     def __init__(self, player_x, player_y, enemy=False):
+        super().__init__()
         self.width = 10
         self.height = 100
         self.speed = 5
@@ -15,11 +22,36 @@ class Player(sprite.Sprite):
             keys = key.get_pressed()
             if keys[K_UP] or keys[K_w]: self.rect.y -= 5
             if keys[K_DOWN] or keys[K_s]: self.rect.y += 5
+        
     def draw_player(self, screen):
+        draw.rect(screen, WHITE, self.rect)
+class Ball(sprite.Sprite):
+    def __init__(self, ball_x, ball_y):
+        super().__init__()
+        self.ball_speed_x = 5
+        self.ball_speed_y = 5
+        self.width = 30
+        self.height = 30
+        self.rect = Rect(ball_x, ball_y, self.width, self.height)
+    def update(self):
+        ball_x += self.ball_speed_x 
+        ball_y += self.ball_speed_y
+    def draw(self, screen):
+        draw.ellipse(screen, WHITE, self.rect)
+
+class Wall(sprite.Sprite):
+    def __init__(self, wall_x, wall_y):
+        super().__init__()
+        self.width = 10
+        self.height = 1100
+        self.rect = Rect(wall_x, wall_y, self.width, self.height)
+    def draw(self, screen):
         draw.rect(screen, WHITE, self.rect)
 
 Player_paddle = Player(25, 300, False)
 Enemy_paddle = Player(1900, 300, True)
+Wall_middle = Wall(CENTER_WALLX, 0)
+Ball_pong = Ball(700, 0)
 run = True
 
 while run:
@@ -28,6 +60,10 @@ while run:
             run = False
     Player_paddle.draw_player(screen)
     Enemy_paddle.draw_player(screen)
+    Wall_middle.draw(screen)
+    Ball_pong.draw(screen)
+
+    Ball_pong.update()
     Player_paddle.update()
     Enemy_paddle.update()
 
